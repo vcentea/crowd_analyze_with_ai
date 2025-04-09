f0d05644db2754a2d6a788c7a061e14462336a0d
vcentea <your.email@example.com>
2025-04-09 09:39:32 +0200

    Update server to use PORT from .env file

--- server/index.ts
+++ server/index.ts
@@ -65,10 +65,8 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = 5000;
+  // Use the PORT from the environment variables or default to 5001
+  const port = process.env.PORT || 5001;
   server.listen({
     port,
     host: "0.0.0.0",
